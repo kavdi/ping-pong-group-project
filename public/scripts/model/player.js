17d607eaad8c9516cbe86093782f85e57ab04dd6
gabrielx52 <gabriel.meringolo@gmail.com>
2017-10-04 12:09:24 -0700

    Added player page controller and player.js

--- public/scripts/model/player.js
+++ public/scripts/model/player.js
@@ -8,7 +8,7 @@ var app = app || {};
     this.user_id = user_id,
     this.wins = 0,
     this.losses = 0,
-    this.games_played =0,
+    this.games_played = 0,
     this.rank = 0,
     this.rival = null
     // this.last_activity = new Date()  // Stretch Goal
@@ -16,24 +16,13 @@ var app = app || {};
 
   Player.all = [];
 
-  Player.loadPlayers = function(callback){
-    $.get('/api/players', function(response) {
-      Player.all = response.map(function(player) {return new Player(player)})
-    }).then(callback, err => console.error(err, 'error'))
-  }
+  Player.loadPlayers = function(){
+    $.get('/api/players', function(request, response) {
+      console.log(request);
+      // console.log(response);
+      // Player.all = response.map(function(player) {return new Player(player)})
+    // }).then(callback, err => console.error(err, 'error'))
+    })}
   module.Player = Player
 }
 )(app)
-
-
-//// for server.js
-
-APP.get('/api/players', function(req, res){
-  CLIENT.query(
-    `SELECT (name, user_id, wins, losses, games_played, rank, rival) FROM player;`).then(
-      function(data){
-        console.log(data)
-        res.send(data)
-      }
-    )
-})
