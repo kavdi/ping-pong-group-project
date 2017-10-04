d66014ee93983181dffce55311ec3e2e98f3f01e
gabrielx52 <gabriel.meringolo@gmail.com>
2017-10-04 11:11:38 -0700

    Added load players function and get listener.

--- public/scripts/model/player.js
+++ public/scripts/model/player.js
@@ -9,12 +9,31 @@ var app = app || {};
     this.wins = 0,
     this.losses = 0,
     this.games_played =0,
-    this.rank = 'unranked',
+    this.rank = 0,
     this.rival = null
+    // this.last_activity = new Date()  // Stretch Goal
   }
-  Player.populatePlayers = function(){
 
+  Player.all = [];
+
+  Player.loadPlayers = function(callback){
+    $.get('/api/players', function(response) {
+      Player.all = response.map(function(player) {return new Player(player)})
+    }).then(callback, err => console.error(err, 'error'))
   }
   module.Player = Player
 }
 )(app)
+
+
+//// for server.js
+
+APP.get('/api/players', function(req, res){
+  CLIENT.query(
+    `SELECT (name, user_id, wins, losses, games_played, rank, rival) FROM player;`).then(
+      function(data){
+        console.log(data)
+        res.send(data)
+      }
+    )
+})
