dc598489e1351cabfc5ac34e0c83500fe0a33e6b
David Franklin <dave5801@gmail.com>
2017-10-05 13:47:08 -0700

    removed old comments, added querries to server.js

--- public/scripts/view/challengerView.js
+++ public/scripts/view/challengerView.js
@@ -4,24 +4,10 @@ var app = app || {};
 
 (function(module){
   const challengerView = {};
-
-  /*
-  playerView.populatePlayers = function(){
-    //MORE TO FOLLOW
-    app.Player.all.forEach(player =>{
-      console.log(player);
-      $('#player-table').append(player.toHtml());
-    });
-  };
-  */
-
   challengerView.showButtons = function(){
     app.Challenger.all.forEach(challenger => {
       //show actual button
-      /**/
       $(`.challengeButton[player_id = '${challenger['id']}']`).show();
-      //default should be hidden
-
     });
   };
 
