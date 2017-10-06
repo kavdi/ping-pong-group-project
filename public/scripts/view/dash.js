aa012cf159d788e4d24cc016ca27ee4d473cf014
kavdihodgson <kavdyjh@gmail.com>
2017-10-05 23:05:51 -0700

    got the dash to work and buttons to hide when challenge present

--- public/scripts/view/dash.js
+++ public/scripts/view/dash.js
@@ -5,74 +5,24 @@ var app = app || {};
 (function(module){
   const dashHandler = {};
 
-  dashHandler.loadDash = function(userObj){
-    $('#dash_name').text(userObj.name);
-    $('#dash_wins').text(userObj.wins);
-    $('#dash_loss').text(userObj.losses);
-    $('#dash_ranking').text(userObj.rank === 11 ? 'Unranked' : userObj.rank);
-    // $('#dash_you_button').text(userObj.name);
-    // $('#dash_them_button').text(userObj.opponent);
-    $('#local_user_dash_button').attr('player-id', userObj.player_id);
-    $('#other_user_dash_button').attr('player-id', userObj.opp_id);
-    if(userObj.challenged === 1){
-      $('.challengeButton').hide();
-      $('#dash_results').show();
-    }
-  }
-
-  dashHandler.loadPlayer = function(callback) {
+  dashHandler.loadPlayer = function(req, res) {
     $.get(`/api/player`, {userId: app.Player.localUser})
-      .then(callback)
+      .then(
+        function(res){
+          console.log(res);
+          $('#dash_name').text(res[0].name);
+          $('#dash_wins').text(res[0].wins);
+          $('#dash_loss').text(res[0].losses);
+          $('#dash_ranking').text(res[0].rank === 11 ? 'Unranked' :'Rank: ' + res[0].rank);
+          $('#local_user_dash_button').attr('player-id', res[0].player_id);
+          $('#other_user_dash_button').attr('player-id', res[0].opp_id);
+          if(res[0].challenged === 1){
+            $('.buttonStyle').hide();
+            $('#dash_results').show();
+          }
+        }
+      )
   };
 
   module.dashHandler = dashHandler;
 })(app)
-
-
-// This needs to go in server.js
-
-
-// APP.get('/api/player', (req, res) => {
-//   CLIENT.query(
-//     `SELECT (name, player_id, rank, wins, losses, games_played, )`
-//   )
-// })
-//
-// `SELECT player_id
-//  FROM player
-//     JOIN player_match
-//       ON player.id
-//         = player_match.id
-//     JOIN match
-//       ON player_match.id
-//         = match.id
-//  `
-//
-//  `SELECT match.id
-//   FROM match
-//     JOIN player_match
-//       ON match.id
-//         = player_match.match_id
-//    WHERE player.id = ${app.Player.localUser} AND match.winner = null;`
-//
-//
-// (`CREATE TABLE IF NOT EXISTS
-//   match (
-//     id SERIAL PRIMARY KEY,
-//     winner INT
-//   );`)
-//   (`CREATE TABLE IF NOT EXISTS
-//     player_match(
-//       player_id INT REFERENCES player(id),
-//       match_id INT REFERENCES match(id)
-//     );`
-//   )
-//
-//   SELECT matches.id
-//    FROM matches
-//      JOIN play_match
-//        ON matches.id
-//          = play_match.match_id
-//     WHERE play_match.players_id = 1 AND matches.winner = null;
-
-// name, player_id, rank, wins, losses, challenged, opp_id)
